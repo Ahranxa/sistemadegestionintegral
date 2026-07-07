5c06099b4df81ff653c9c6e1220478e57a8b6ce7
Ahranxa <p252666@uach.mx>
2026-07-07 13:31:03 -0600

    fix/clerk-server-keys

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,6 @@
 import { withClerkHandler } from 'svelte-clerk/server';
 
-export const handle = withClerkHandler();
+export const handle = withClerkHandler({
+	publishableKey: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
+	secretKey: process.env.CLERK_SECRET_KEY
+});
